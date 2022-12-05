19b9cedd29c618dfdf7d05e12f08aad51b0de109
Nikola Crnogorac <nikola.crnogorac@vicert.com>
2022-12-05 12:24:03 +0100

    Fixed dagger issue

--- app/src/main/java/com/vicert/his/presentation/login/LoginViewModelProviderFactory.kt
+++ app/src/main/java/com/vicert/his/presentation/login/LoginViewModelProviderFactory.kt
@@ -5,13 +5,12 @@ import androidx.lifecycle.ViewModelProvider
 import com.vicert.his.repository.LoginRepository
 import javax.inject.Singleton
 
-@Singleton
 class LoginViewModelProviderFactory(
     val loginRepository: LoginRepository
 ) : ViewModelProvider.Factory {
 
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            return LoginActivityViewModel(loginRepository) as T
+            return LoginActivityViewModel() as T
     }
 }
